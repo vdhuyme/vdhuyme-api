fd8c841aff0bb2e0ecda789904ebe936470c1c80
Vo Duc Huy <voduchuy2001@gmail.com>
2025-05-16 14:13:55 +0700

    update: change from sqlite3 to pg

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,8 +12,12 @@ const basePath: string = __dirname
 
 const config: Record<NodeEnvironment, DataSourceOptions> = {
   development: {
-    type: 'sqlite',
-    database: 'development.sqlite',
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT || '5432'),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     synchronize: true,
     logging: true,
     entities: [path.join(basePath, 'entities', '**', '*{.ts,.js}')],
@@ -21,18 +25,26 @@ const config: Record<NodeEnvironment, DataSourceOptions> = {
     subscribers: [path.join(basePath, 'subscribers', '**', '*{.ts,.js}')]
   },
   production: {
-    type: 'sqlite',
-    database: 'production.sqlite',
-    synchronize: true,
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT || '5432'),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: false,
     logging: true,
     entities: [path.join(basePath, 'entities', '**', '*{.ts,.js}')],
     migrations: [path.join(basePath, 'migrations', '**', '*{.ts,.js}')],
     subscribers: [path.join(basePath, 'subscribers', '**', '*{.ts,.js}')]
   },
   test: {
-    type: 'sqlite',
-    database: ':memory:',
-    synchronize: true,
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT || '5432'),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: false,
     logging: false,
     entities: [path.join(basePath, 'entities', '**', '*{.ts,.js}')],
     migrations: [],
@@ -48,14 +60,15 @@ logger.info(`Database configuration for ${env}:`, {
 
 export const db = new DataSource(config[env])
 
+const driver = process.env.DB_CONNECTION
 export const database = async (): Promise<void> => {
   try {
     await db.initialize()
-    logger.info(`[${env}]📦 SQLite database connected`)
+    logger.info(`[${env}]📦 ${driver} database connected`)
 
     logger.info(`Connected to database: ${config[env].database}`)
   } catch (error: any) {
-    logger.error(`❌ Failed to connect with SQLite: ${error.message}`)
+    logger.error(`❌ Failed to connect with ${driver}: ${error.message}`)
     logger.error(`Stack trace: ${error.stack}`)
     throw error
   }
