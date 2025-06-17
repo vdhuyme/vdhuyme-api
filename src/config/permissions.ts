d1bbfe93ceee876223459576c2b7964ed9c8d99e
Vo Duc Huy <voduchuy2001@gmail.com>
2025-06-17 09:22:16 +0700

    fix: production

--- src/config/permissions.ts
+++ src/config/permissions.ts
@@ -1,9 +1,24 @@
-import fs from 'fs'
-
-import { storage } from '@utils/storage'
-
-const permissionsPath = storage.storagePath('data/roles-permissions.json')
-
-export const ROLES_PERMISSIONS: Record<string, string[]> = JSON.parse(
-  fs.readFileSync(permissionsPath, 'utf-8')
-)
+export const ROLES_PERMISSIONS: Record<string, string[]> = {
+  admin: [
+    'user.read',
+    'user.update',
+    'post.create',
+    'post.update',
+    'post.delete',
+    'post.read',
+    'tag.create',
+    'tag.update',
+    'tag.delete',
+    'tag.read',
+    'category.create',
+    'category.update',
+    'category.delete',
+    'category.read',
+    'comment.update',
+    'comment.delete',
+    'comment.read',
+    'file.upload',
+    'stats.ga4'
+  ],
+  user: []
+}
